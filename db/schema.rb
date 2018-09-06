4639eef5ff6fe807311d8b2a8a00b3efd7f25ebe
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2018-09-06 17:41:44 +0300

    Add user name and email field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_185616) do
+ActiveRecord::Schema.define(version: 2018_09_06_142205) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_05_185616) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
